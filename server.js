8fba5c1bfd41c6cf72a41c95336f4834f1e0791f
prasadk418 <prasad.kandubothu@gmail.com>
2017-03-14 22:51:21 +0530

    2nd commit

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express=require('express');
 var app=express();
 
+app.get('/', function(req, res){
+res.send('this is output from node js');
+});
+
 app.listen(2030, function(){
 	console.log('server is running on port 2030');
 })
